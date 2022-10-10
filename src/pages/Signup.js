84264681c220d86e4174de1771c7146b9cdac2f0
Cécile Deshayes-Guinard <cecile.deshayes.guinard@gmail.com>
2022-10-10 21:25:34 +0200

    signup maj

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -6,91 +6,93 @@ const API_URL = "http://localhost:5005";
 
 function Signup(props) {
 
-    const [userName, setUserName] = useState("");
-    const [email, setEmail] = useState("");
-    const [phoneNumber, setPhoneNumber] = useState("");
-    const [password, setPassword] = useState("");
-    const [errorMessage, setErrorMessage] = useState(undefined);
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(undefined);
+  const navigate = useNavigate();
   
-    const navigate = useNavigate();
-    
-    const handleUserName = (e) => setUserName(e.target.value);
-    const handleEmail = (e) => setEmail(e.target.value);
-    const handlePhoneNumber = (e) => setPhoneNumber(e.target.value);
-    const handlePassword = (e) => setPassword(e.target.value);
+  const handleUserName = (e) => setUserName(e.target.value);
+  const handleEmail = (e) => setEmail(e.target.value);
+  const handlePhoneNumber = (e) => setPhoneNumber(e.target.value);
+  const handlePassword = (e) => setPassword(e.target.value);
+
   
-    
-    const handleSignupSubmit = (e) => {
-      e.preventDefault(); // permet de retenir l'envoi POST
-
-      const requestBody = { userName, phoneNumber, email, password };
-   
-      axios.post(`${API_URL}/auth/signup`, requestBody) // axios POST vers le server
-        .then((response) => {
-          navigate('/login'); // redirection vers la page login après validation du formulaire
-        })
-        .catch((error) => {
-          const errorDescription = error.response.data.message;
-          setErrorMessage(errorDescription);
-        })
-    };
-    
-    return (
-      <div className="startPages signup">
-
-        <form onSubmit={handleSignupSubmit}>
-          <div className="startBlock">
+  const handleSignupSubmit = (e) => {
+    e.preventDefault(); // permet de retenir l'envoi POST
+    const requestBody = { userName, phoneNumber, email, password };
+ 
+    axios.post(`${API_URL}/auth/signup`, requestBody) // axios POST vers le server
+      .then((response) => {
+        navigate('/login'); // redirection vers la page login après validation du formulaire
+      })
+      .catch((error) => {
+        const errorDescription = error.response.data.message;
+        setErrorMessage(errorDescription);
+      })
+  };
+  
+  return (
+    <div className="startPages signup">
+      <form onSubmit={handleSignupSubmit}>
+        <div className="startBlock">
           <h3>Mes informations !</h3>
 
-            <input 
-              type="text"
-              name="userName"
-              value={userName}
-              placeholder="Pseudo"
-              onChange={handleUserName}
-            />
+          <input 
+            type="text"
+            name="userName"
+            value={userName}
+            placeholder="Pseudo"
+            onChange={handleUserName}
+          >
+          { errorMessage && <p className="error-message">{errorMessage}</p> }
+          </input>
+          
+          <input 
+            type="email"
+            name="email"
+            value={email}
+            placeholder="Email"
+            onChange={handleEmail}
+          />
+          { errorMessage && <p className="error-message">{errorMessage}</p> }
+
+          <input 
+            type="text"
+            name="phoneNumber"
+            value={phoneNumber}
+            placeholder="Téléphone"
+            onChange={handlePhoneNumber}
+          />
+          { errorMessage && <p className="error-message">{errorMessage}</p> }
   
-            <input 
-              type="email"
-              name="email"
-              value={email}
-              placeholder="Email"
-              onChange={handleEmail}
-            />
+          <input 
+            type="password"
+            name="password"
+            value={password}
+            placeholder="Mot de passe"
+            onChange={handlePassword}
+          />
+          { errorMessage && <p className="error-message">{errorMessage}</p> }
+        </div>
+          
+        <div className="logs">
+          <Link to="/loading">
+            <button className="button buttonsBlack">Retour</button>
+          </Link>
+          <button className="button buttonsBlack" type="submit">Enregistrer !</button>
+        </div>
+      </form>
+
+      { errorMessage
+      && <p className="error-message">{errorMessage}</p>
+      && <p>Already have account?</p>
+      && <Link to="/signup">
+           <button className="button buttonsWhite">Créer son compte !</button>
+         </Link> }
+    </div>
+  )
+}
   
-            <input 
-              type="text"
-              name="phoneNumber"
-              value={phoneNumber}
-              placeholder="Téléphone"
-              onChange={handlePhoneNumber}
-            />
-    
-            <input 
-              type="password"
-              name="password"
-              value={password}
-              placeholder="Mot de passe"
-              onChange={handlePassword}
-            />
-            </div>
-  
-            <div className="logs">
-            <Link to="/loading">
-              <button className="button buttonsBlack">Retour</button>
-            </Link>
-            <button className="button buttonsBlack" type="submit">Enregistrer !</button>
-          </div>
-        </form>
-  
-        { errorMessage
-        && <p className="error-message">{errorMessage}</p>
-        && <p>Already have account?</p>
-        && <Link to="/signup">
-             <button className="button buttonsWhite">Créer son compte !</button>
-           </Link> }
-      </div>
-    )
-  }
-  
-  export default Signup;
+export default Signup;
